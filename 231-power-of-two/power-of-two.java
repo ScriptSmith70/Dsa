535717638816c2dc6d492733aedba1905ca8dcee
ScriptSmith70 <insidestory100@gmail.com>
2025-08-10 13:39:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (25.91%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,3 +3,18 @@ class Solution {
         return n > 0 && (n & (n - 1)) == 0;
     }
 }
+
+/*
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        if (n <= 0) return false;
+
+        for(int x=0;x<31;x++){
+        if(n==Math.pow(2, x)){
+            return true;
+          }
+        }
+        return false;
+    }
+*/
