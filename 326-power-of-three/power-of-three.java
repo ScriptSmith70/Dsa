eed0b5978bf2f52c67827af738615209ccdff9a2
ScriptSmith70 <insidestory100@gmail.com>
2025-08-11 09:27:18 +0530

    Time: 15 ms (5.86%) | Memory: 44.6 MB (10.81%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -7,3 +7,15 @@ class Solution {
         return false;
     }
 }
+/*
+class Solution {
+
+    public boolean isPowerOfThree(int n) {
+
+        // 3^19 = 1162261467 
+
+        return n > 0 && (1162261467  % n) == 0;
+        
+    }
+}
+*/
