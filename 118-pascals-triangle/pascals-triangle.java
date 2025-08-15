bb6be6cb55e93891c53ef8f9e901f80833707db9
ScriptSmith70 <insidestory100@gmail.com>
2025-08-15 10:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (78.73%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,18 +1,22 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascal = new ArrayList<>();
-
-        for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>(Collections.nCopies(i + 1, 1));
-            
-            for (int j = 1; j < i; j++) {
-                int val = pascal.get(i - 1).get(j - 1) + pascal.get(i - 1).get(j);
-                row.set(j, val);
-            }
-            pascal.add(row);
+       List<List<Integer>> result = new ArrayList<>();
+       for(int row=1;row<=numRows;row++){
+        result.add(nCr(row));
+       }
+       return result;
+    }
+    private List<Integer> nCr(int row){
+        List<Integer> temp = new ArrayList<>();
+        int ans =1;
+        temp.add(ans);
+        for(int col=1;col<row;col++){
+            ans = ans*(row-col);
+            ans = ans/col;
+            temp.add(ans);
         }
-        return pascal;
+        return temp;
+        
+
     }
 }
