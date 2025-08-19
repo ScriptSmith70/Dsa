19e6cefced93f2a4f6f5f3f1675eb0896f659cb8
ScriptSmith70 <insidestory100@gmail.com>
2025-08-19 14:38:54 +0530

    Time: 1 ms (86.15%) | Memory: 41.9 MB (78.95%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-       List<List<Integer>> result = new ArrayList<>();
-       for(int row=1;row<=numRows;row++){
-        result.add(nCr(row));
-       }
-       return result;
-    }
-    private List<Integer> nCr(int row){
-        List<Integer> temp = new ArrayList<>();
-        int ans =1;
-        temp.add(ans);
-        for(int col=1;col<row;col++){
-            ans = ans*(row-col);
-            ans = ans/col;
-            temp.add(ans);
-        }
-        return temp;
-        
+        List<List<Integer>> result = new ArrayList<>();
 
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);  // first element is always 1
+
+            for (int j = 1; j < i; j++) {
+                // sum of two numbers above it
+                int val = result.get(i - 1).get(j - 1) + result.get(i - 1).get(j);
+                row.add(val);
+            }
+
+            if (i > 0) {
+                row.add(1); // last element is always 1 (except first row)
+            }
+
+            result.add(row);
+        }
+
+        return result;
     }
 }
