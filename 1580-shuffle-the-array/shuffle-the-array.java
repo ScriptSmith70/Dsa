8aa40b5e94cc1415aee6e53e7aaf0b970cf2e484
ScriptSmith70 <insidestory100@gmail.com>
2025-08-24 18:15:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (59.28%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] result = new int[2 * n];
-        int index = 0;
         
         for (int i = 0; i < n; i++) {
-            result[index++] = nums[i];     // xᵢ
-            result[index++] = nums[i + n]; // yᵢ
+            result[2 * i] = nums[i];       // place xᵢ at even position
+            result[2 * i + 1] = nums[i+n]; // place yᵢ at odd position
         }
         
         return result;
