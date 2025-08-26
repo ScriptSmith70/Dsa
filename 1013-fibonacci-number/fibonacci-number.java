e3c2eecc9e3f1500742d0a3ad12a14c87d8bbad5
ScriptSmith70 <insidestory100@gmail.com>
2025-08-26 12:31:10 +0530

    Time: 8 ms (45.10%) | Memory: 40.3 MB (64.04%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,3 +1,5 @@
+// Recursion
+
 class Solution {
     public int fib(int n) {
         if(n<=1) {
@@ -8,3 +10,15 @@ class Solution {
         }
     }
 }
+
+/* Math Formula
+
+class Solution {
+    public int fib(int n) {
+        double sqrt5 = Math.sqrt(5);
+        double fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
+        return (int) Math.round(fibN / sqrt5);
+    }
+}
+
+*/
