7b46c7abb479c0a13645c1344fd6e03b78636677
ScriptSmith70 <insidestory100@gmail.com>
2025-08-29 13:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (27.97%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -5,9 +5,14 @@ class Solution {
 
         while (start <= end) {
             int mid = start + (end-start)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid-1;
-            else start = mid+1;
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                end = mid-1;
+            }
+            else{start = mid+1;
+            }
         }
 
         return start;
