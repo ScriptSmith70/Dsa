053d3d9d135e800317493fad3ecba6a12e4b4df3
ScriptSmith70 <insidestory100@gmail.com>
2025-08-30 14:49:54 +0530

    Time: 1 ms (98.81%) | Memory: 43 MB (53.73%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,15 +1,38 @@
+/* Three ways to count digits in a number 
+
+Using Division (while loop) 
+static int digits(int num) {
+    int count = 0;
+    while (num > 0) {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
+
+Using String Conversion
+static int digits(int num) {
+    return String.valueOf(num).length();
+}
+
+Using Logarithm
+static int digits(int num) {
+    return (int) (Math.log10(num) + 1);
+}
+
+*/
 class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (digits(nums[i]) % 2 == 0) { // call helper method
+            if (digits(nums[i]) % 2 == 0) {
                 count++;
             }
         }
         return count;
     }
 
-    // helper method to count digits
+
     static int digits(int num) {
         int count = 0;
         while (num > 0) {
