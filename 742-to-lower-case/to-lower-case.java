7b27beb9c7a1b8f690bd954fc4d22734b2ed99b5
ScriptSmith70 <insidestory100@gmail.com>
2025-09-01 18:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (43.04%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,23 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        String lower=s.toLowerCase();   //Java inbuilt function
+     return lower;
     }
 }
+
+//Method 2: without toLowerCase
+// String lower="";
+//for(int i=0; i<s.length(); i++){
+//     char ch= s.charAt(i);
+
+// If character is uppercase (A-Z), convert it to lowercase
+//     if(ch>= 'A' && ch<='Z'){
+
+// Add 32 to ASCII value to get corresponding lowercase
+//         ch= (char)(ch+ 32);
+//     }
+
+// Append the lowercase character to the result
+//     lower= lower + ch;
+// } 
+//     return lower;
