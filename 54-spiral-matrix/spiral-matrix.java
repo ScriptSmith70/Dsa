4629a29af921dc48112a458efd556db40dcaecbc
ScriptSmith70 <insidestory100@gmail.com>
2025-09-21 20:02:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (64.98%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,33 +1,38 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        int top = 0;
-        int bottom = matrix.length -1;
-        int left = 0;
-        int right = matrix[0].length - 1;
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
 
-        while (top <= bottom && left <= right){
-            for (int j = left; j <= right; j++){
-                result.add(matrix[top][j]);
+        while (top <= bottom && left <= right) {
+            // Top row
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
             }
             top++;
-            for (int i = top; i <= bottom; i++){
-                result.add(matrix[i][right]);
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
             }
-            right --;
-            if (top <= bottom){
-                for (int j = right; j >=left; j--){
-                    result.add(matrix[bottom][j]);
+            right--;
+
+            if (top <= bottom) {
+                // Bottom row
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
                 }
-                bottom --;
+                bottom--;
             }
-            if (left <= right){
-                for (int i = bottom; i >= top; i--){
-                    result.add(matrix[i][left]);
+
+            if (left <= right) {
+                // Left column
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
                 }
-                left ++;
+                left++;
             }
         }
-        return result;
+        return res;
     }
 }
